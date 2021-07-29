fc8817c926ef205cf2d9722f4ac94041185824c6
Christian Zamora <christianzamoraburgos@gmail.com>
2021-07-28 22:33:39 -0500

    added navbar images and path to new worker

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   attr_writer :login
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, #:trackable,
+         :recoverable, :rememberable, 
          :validatable, authentication_keys: [:login]
 
   has_many :workers
