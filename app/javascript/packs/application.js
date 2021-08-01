5196a451fef805c3efc789e0961b6523084b001b
Christian Zamora <christianzamoraburgos@gmail.com>
2021-08-01 00:25:26 -0500

    changed alert.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -32,3 +35,4 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
